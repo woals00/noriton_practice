30a3927982e84e6123bf1c37a2b6d986610e883a
shine-jung <79754613+shine-jung@users.noreply.github.com>
2023-01-09 16:06:02 +0900

    Inital Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# food-handong-server
+ 포항 음식 커뮤니티 웹 서비스 서버
---
+++ src/main/java/com/webcamp5/foodhandongserver/FoodHandongServerApplication.java
@@ -0,0 +1,13 @@
+package com.webcamp5.foodhandongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHandongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodHandongServerApplication.class, args);
+    }
+
+}
