17f0697816663008aa8e18cbbe812e16c303139a
jaeminjeong <21900675@handong.edu>
2023-01-16 18:07:37 +0900

    add LoginCheckInterceptor.java

---
+++ src/main/java/com/webcamp5/foodhandongserver/util/LoginCheckInterceptor.java
@@ -0,0 +1,26 @@
+package com.webcamp5.foodhandongserver.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("login");
+        if (obj == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return false;
+        }
+//        System.out.println("AAAAA");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
